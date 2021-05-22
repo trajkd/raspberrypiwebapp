ddb9374c938a55fb49226db2dde08b83de7182d2
trajkd <trajkd@gmail.com>
2021-05-22 13:08:54 +0200

    Write to Firestore

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+from datetime import datetime
+
+cred = credentials.Certificate("./ServiceAccountKey.json")
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		return "Hello"
+		now = datetime.now()
+		doc_ref = db.collection(u'data').document(u'person'+now.strftime("%d%m%Y|%H:%M:%S"))
+		doc_ref.set({
+			u'name': request.form['name'], 
+			u'surname': request.form['surname']
+			}, merge=True)
+		return request.form['name'] + " " + request.form['surname'] + " succesfully added."
 	return render_template('index.html')
 
 if __name__ == '__main__':
